4569ac0b7465cafb356fd685076bd3752d3b336d
Manish <manishdas11811480@gmail.com>
2024-06-13 16:03:13 +0530

    first commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAme65iNCWZMdphhPrZBvw75uGR_1AErE0",
-  authDomain: "wavv-music.firebaseapp.com",
-  projectId: "wavv-music",
-  storageBucket: "wavv-music.appspot.com",
-  messagingSenderId: "245569092931",
-  appId: "1:245569092931:web:190b3215a950fe5a82c3f6",
-  measurementId: "G-2K7452MNJ7"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
